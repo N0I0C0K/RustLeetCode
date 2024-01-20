60b5a5028e51f693a744f935ed31f3615dee4264
N0I0C0K <nick131410@aliyun.com>
2024-01-20 19:25:54 +0800

    every day

--- src/easy.rs
+++ src/easy.rs
@@ -1,54 +1,22 @@
 struct Solution;
 
 impl Solution {
-    pub fn min_operations_max_profit(
-        customers: Vec<i32>,
-        boarding_cost: i32,
-        running_cost: i32,
-    ) -> i32 {
-        if boarding_cost * 4 < running_cost {
-            return -1;
-        }
-        let (mut res, mut tmax) = (-1, -1);
-        let (mut lun, mut cur) = (0, 0);
-        let mut has = 0;
-
-        for (_, x) in customers.iter().enumerate() {
-            has += *x as i32;
-            let tm = has.min(4);
-            cur += tm * boarding_cost;
-            has -= tm;
-
-            lun += 1;
-            cur -= running_cost;
-
-            if cur > tmax {
-                (res, tmax) = (lun, cur);
-            }
-        }
-        while has > 0 {
-            let tm = has.min(4);
-            cur += tm * boarding_cost;
-            has -= tm;
-
-            lun += 1;
-            cur -= running_cost;
-
-            if cur > tmax {
-                (res, tmax) = (lun, cur);
-            }
-        }
-        return res;
+    pub fn split_words_by_separator(words: Vec<String>, separator: char) -> Vec<String> {
+        return words
+            .iter()
+            .map(|x| {
+                x.split(separator)
+                    .filter(|t| t.len() > 0)
+                    .map(|t| String::from(t))
+                    .collect::<Vec<String>>()
+            })
+            .collect::<Vec<Vec<String>>>()
+            .concat();
     }
 }
 
 #[cfg(test)]
 mod test {
-    use super::Solution;
-
     #[test]
-    fn test_1() {
-        let res = Solution::min_operations_max_profit(vec![8, 3], 5, 6);
-        assert_eq!(res, 3);
-    }
+    fn test1() {}
 }
