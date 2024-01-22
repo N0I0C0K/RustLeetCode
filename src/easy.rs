1727b7864434feb9f9bf002f44f4a3b9261fb6cf
N0I0C0K <nick131410@aliyun.com>
2024-01-22 18:22:02 +0800

    update

--- src/easy.rs
+++ src/easy.rs
@@ -1,22 +1,43 @@
 struct Solution;
 
 impl Solution {
-    pub fn split_words_by_separator(words: Vec<String>, separator: char) -> Vec<String> {
-        return words
-            .iter()
-            .map(|x| {
-                x.split(separator)
-                    .filter(|t| t.len() > 0)
-                    .map(|t| String::from(t))
-                    .collect::<Vec<String>>()
-            })
-            .collect::<Vec<Vec<String>>>()
-            .concat();
+    pub fn maximum_swap(num: i32) -> i32 {
+        let mut digits = num
+            .to_string()
+            .chars()
+            .enumerate()
+            .collect::<Vec<(usize, char)>>();
+        digits.sort_by_key(|x| (x.1 as u8, x.0));
+        let mut res = num.to_string().chars().collect::<Vec<char>>();
+        let mut t = '0';
+        let mut m = '0';
+        for i in 0..digits.len() {
+            if res[i] != digits[digits.len() - i - 1].1 {
+                //res.swap(i, digits[digits.len() - i - 1].0);
+                t = res[i];
+                m = digits[digits.len() - i - 1].1;
+                res[i] = digits[digits.len() - i - 1].1;
+                break;
+            }
+        }
+        for i in (0..digits.len()).rev() {
+            if res[i] == m {
+                res[i] = t;
+                break;
+            }
+        }
+
+        res.iter().collect::<String>().parse().unwrap()
     }
 }
 
 #[cfg(test)]
 mod test {
+    use super::Solution;
+
     #[test]
-    fn test1() {}
+    fn test1() {
+        let res = Solution::maximum_swap(98368);
+        assert_eq!(res, 98863);
+    }
 }
